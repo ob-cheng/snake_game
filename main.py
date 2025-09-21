1f6c4556d2690f18d34d3eccce05d0ac9a22a774
ob_cheng <obadiahcheng@icloud.com>
2025-09-21 20:54:40 +0200

    Refactor game logic by encapsulating snake behavior into a `Snake` class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Screen, Turtle
+from snake import Snake
 
 screen = Screen()
 screen.setup(600, 600)
@@ -7,25 +8,13 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-segments = []
-
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-for position in starting_position:
-    new_cube = Turtle("square")
-    new_cube.color("white")
-    new_cube.penup()
-    new_cube.goto(position)
-    segments.append(new_cube)
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num-1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
